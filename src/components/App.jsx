f791cc9533d2a5b89ee683ddd5a916e52b9744fa
Senichkaa <super-kapanaiko406@ukr.net>
2023-08-24 22:47:50 +0300

    try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,32 @@
 import { Component } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
   };
 
+  addContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        {
+          id: nanoid(),
+          ...newContact,
+        },
+      ],
+    }));
+  };
+
   render() {
-    return <ContactForm />;
+    <h2>Phonebook</h2>;
+    return <ContactForm onAdd={this.addContact} />;
   }
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,25 @@
 import { Formik, Field, Form } from 'formik';
-export const ContactForm = () => {
+// import * as Yup from 'yup';
+
+// const contactSchema = Yup.object().shape({
+//   name: Yup.string.min(2, 'Too short name!').required('required'),
+// });
+
+export const ContactForm = ({ onAdd }) => {
   return (
-    <Formik initialValues={{ name: '', email: '' }} onSubmit={values => {}}>
+    <Formik
+      initialValues={{ name: '' }}
+      //   validationSchema={contactSchema}
+      onSubmit={(values, actions) => {
+        onAdd(values);
+        actions.resetForm();
+      }}
+    >
       <Form>
         <Field name="name" type="text" />
-        <Field name="email" type="email" />
-        <button type="submit">Submit</button>
+
+        {/* <Field name="number" type="number" /> */}
+        <button type="submit">Add contact</button>
       </Form>
     </Formik>
   );
